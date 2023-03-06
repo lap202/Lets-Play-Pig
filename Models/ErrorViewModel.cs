b617a9fbe2e30b687bd9e4e7d79c820ce5a19cc5
Andrew Bach <Andrew.Bach@southeasttech.edu>
2023-03-05 20:11:34 -0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BacA_Exam2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
